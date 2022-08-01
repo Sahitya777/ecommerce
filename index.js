02a9ca2bd575004cc2010e0d26a078cacc6ee0c0
Sahitya77 <sahityanijhawan@gmail.com>
2022-08-01 13:54:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+var express = require('express')
+var ejs = require('ejs')
+var bodyParser=require('body-parser')
+var mysql=require('mysql')
+var app = express();
+var session=require('express-session')
+
+
+mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"node_project"
+})
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.listen(8080);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({secret:"secret"}))
+
+
+
+
+function isProductInCart(cart,id){
+    for(let i=0;i<cart.length;i++){
+        if(cart[i].id==id){
+            return true;
+        }
+    }
+    return false;
+}
+
+function calculateTotal(cart,req){
+    total=0;
+    for(let i=0;i<cart.length;i++){
+        if(cart[i].sale_prize){
+            total+=(cart[i].sale_prize * cart[i].quantity);
+        }
+        else{
+            total+=(cart[i].prize * cart[i].quantity);
+        }
+    }
+    req.session.total=total;
+    return total;
+}
+
+app.get('/',function(req,res){
+    var con =mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"node_project"
+    })
+
+    con.query("SELECT * FROM products",(err,result)=>{
+
+        res.render('pages/index',{result:result});
+      
+    })
+});
+
+
+app.post('/add_to_cart',function(req,res){
+
+    var id = req.body.id;
+    var name = req.body.name;
+    var prize = req.body.prize;
+    var sale_prize = req.body.sale_prize;
+    var quantity = req.body.quantity;
+    var image = req.body.image;
+    var product = {id:id,name:name,prize:prize,sale_prize:sale_prize,quantity:quantity,image:image};
+ 
+ 
+    if(req.session.cart){
+          var cart = req.session.cart;
+ 
+          if(!isProductInCart(cart,id)){
+             cart.push(product);
+          }
+    }else{
+ 
+       req.session.cart = [product]
+       var cart = req.session.cart;
+ 
+    }
+ 
+ 
+    //calculate total
+    calculateTotal(cart,req);
+ 
+    //return to cart page
+    res.redirect('/cart');
+ 
+ });
+ 
+ 
+ 
+ 
+ app.get('/cart',function(req,res){
+ 
+    var cart = req.session.cart;
+    var total = req.session.total;
+    cart.forEach(item => {
+        console.log(item.prize);
+    });
+ 
+    res.render('pages/cart',{cart:cart,total:total});
+ 
+ 
+ });
+
+ app.post('/remove_product',function(req,res){
+
+    var id = req.body.id;
+    var cart = req.session.cart;
+ 
+    for(let i=0; i<cart.length; i++){
+       if(cart[i].id == id){
+          cart.splice(cart.indexOf(i),1);
+       }
+    }
+ 
+    //re-calculate
+    calculateTotal(cart,req);
+    res.redirect('/cart');
+ 
+ });
+ 
